61ba66110287dd31007780b7e7a012b4fa96e388
河端佑真 <>
2018-05-22 23:39:39 +0900

    pagination

--- getHentaiImg.py
+++ getHentaiImg.py
@@ -2,6 +2,7 @@
 import os
 import os.path
 
+from time import sleep
 from urllib import request
 from bs4 import BeautifulSoup
 
@@ -10,28 +11,38 @@ def derectory_creation(dir_name):
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name)
 
-def hentai_search(url, dir_name="images/"):
+def hentai_search(dir_name="images/"):
+    html_list = []
     imgs_list = []
-    html = request.urlopen(url)
+    getpage = 10
 
-    soup = BeautifulSoup(html, "html.parser")
-    images = soup.find_all("img")
+    for page in range(getpage):
+        sleep(1)
+        print(getpage - page)
+        url = "http://www.dmm.co.jp/digital/videoc/-/list/=/sort=ranking/page={}/".format(page)
+        html_list.append(request.urlopen(url))
 
-    for image in images:
-        if image.get("src").endswith(".jpg"):
-            imgs_list.append(image.get("src"))
+    for html in html_list:
+        soup = BeautifulSoup(html, "html.parser")
+        body = soup.body
+        images = body.find_all("img")
+        for image in images:
+            if image.get("src").endswith(".jpg")\
+            or image.get("src").endswith(".png"):
+                imgs_list.append(image.get("src"))
 
-    derectory_creation(dir_name)
-    for target in imgs_list:
-        re = request.urlopen(target).read()
-        with open(dir_name + target.split("/")[-1], "wb") as f:
-            f.write(re)
+        derectory_creation(dir_name)
+        for target in imgs_list:
+            re = request.urlopen(target).read()
+            sleep(1)
+            with open(dir_name + target.split("/")[-1], "wb") as f:
+                f.write(re)
 
 if __name__ == "__main__":
     #http_test
-    url = "http://www.dmm.co.jp/digital/"
+    #url = "http://www.dmm.co.jp/digital/videoc/-/list/=/sort=ranking/page=2/"
     #https_test
-    #url = "https://official.ameba.jp/categories/14/ranking"
-    hentai_search(url)
+    #url = "https://lastidol.com/"
+    hentai_search()
 
 
